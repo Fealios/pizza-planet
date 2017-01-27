db9ce808663aec9211f2968ce3223d6407e471da
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-01-27 09:10:17 -0800

    add more prototype functions for checking cost and making cost flat rate if the pizza is a plain cheese

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,40 @@ var sizeInput;
 function Pizza(toppings, size){
   this.toppings = toppings; //this will be an array
   this.size = size; //this will be a string
+  this.cost = 6;
 }
 
-Pizza.prototype.checkCost = function(){
-  var toppingsCost = (this.toppings.length * .75);
-  var sizeCost;
+Pizza.prototype.checkCost = function(){ //totals cost based on toppings and size
+  this.toppingsCost = (this.toppings.length * .50);
+  this.checkCheese();
+  this.sizeCost;
   if(size === "small"){
     sizeCost = 6;
   } else if (size === "medium"){
     sizeCost = 8
   } else if (size === "large"){
-    sizeCost = 10
+    sizeCost = 10;
   }
-  return (sizeCost + toppingsCost);
+  this.cost = (sizeCost + toppingsCost);
+}
+
+Pizza.prototype.checkCheese = function(){ //this function makes a plain cheese just cost the size
+  if (toppingsInput.contains("cheese")){
+    this.toppingsCost = 0;
+  }
 }
 
 
 
 
-
-
 $(document).ready(function(){
-
+  $('#pizzaOrder').submit(function(event){
+    event.preventDefault();
+    sizeInput = ($('#sizeChoice option:selected').val());
+    $('input:checkbox[name=toppings]:checked').each(function(){
+      toppingsInput.push($(this).val())
+    });
+    newOrder = new Pizza(toppingsInput, sizeInput);
+    console.log(toppingsInput);
+  })
 })
